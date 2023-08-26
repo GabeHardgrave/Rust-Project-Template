51befc36a5b8b225aa3211f77bd061252405031c
Gabriel <gabriel.hardgrave@gmail.com>
2023-08-26 09:33:16 -0400

    Add scripts for setup, linting, and updating deps

---
+++ script/setup-local-dev.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+rustup toolchain install \
+    nightly \
+    beta
+
+rustup component add \
+    clippy \
+    rustfmt
+
+cargo install \
+    cargo-watch \
+    cargo-udeps --locked
